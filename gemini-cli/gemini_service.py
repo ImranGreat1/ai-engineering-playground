334f0543463db0b69f239dde4d3733cb8a6c73b9
Imran Suleiman <imrangreatest1@gmail.com>
2025-10-30 14:03:26 +0100

    First commit

---
+++ gemini-cli/gemini_service.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+client = genai.Client()
+
+def chat_with_gemini(prompt):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
+
+
+def stream_chat_with_gemini(prompt):
+    stream = client.models.generate_content_stream(
+        model="gemini-2.5-flash-lite",
+        contents=prompt
+    )
+
+    for chunk in stream:
+        yield chunk.text
+
+
+def chat_with_a_cat(prompt):
+    stream = client.models.generate_content_stream(
+        model="gemini-2.5-flash-lite",
+        config=types.GenerateContentConfig(
+            system_instruction="You are a cat. Your name is Ruby"
+        ),
+        contents=prompt
+    )
+
+    for chunk in stream:
+        yield chunk.text
+
+
+def discuss_with_gemini(prompt, prevChat):
+    if not prevChat:
+        chat = client.chats.create(model="gemini-2.5-flash-lite")
+    else:
+        chat = prevChat
+
+    response = chat.send_message(prompt)
+    return { "text": response.text, "chat": chat }
+
+
+def discuss_with_gemini_stream(prompt, prevChat):
+    if not prevChat:
+        chat = client.chats.create(model="gemini-2.5-flash-lite")
+    else:
+        chat = prevChat
+
+    response = chat.send_message_stream(prompt)
+
+    for chunk in response:
+        yield { "text": chunk.text, "chat": chat }
+    
---
+++ gemini-cli/main.py
@@ -0,0 +1,63 @@
+from gemini_service import chat_with_gemini, stream_chat_with_gemini, chat_with_a_cat, discuss_with_gemini, discuss_with_gemini_stream
+
+
+print("Chat with Gemini LLM in the command line...")
+
+
+# while True:
+#     prompt = input("You: ")
+#     if prompt.lower() == "exit":
+#         break
+
+#     response = chat_with_gemini(prompt=prompt)
+#     print(f'gemini: {response}')
+
+
+# while True:
+#     print("")
+#     prompt = input("You: ")
+#     if prompt.lower() == "exit":
+#         break
+
+#     response = stream_chat_with_gemini(prompt=prompt)
+#     print(f"gemini: ", end="")
+#     for chunk_text in response:
+#         print(chunk_text, end="")
+
+
+# while True:
+#     print("")
+#     prompt = input("You: ")
+#     if prompt.lower() == "exit":
+#         break
+
+#     response = chat_with_a_cat(prompt=prompt)
+#     print(f"gemini: ", end="")
+#     for chunk_text in response:
+#         print(chunk_text, end="")
+
+# chat = None
+# while True:
+#     print("")
+#     prompt = input("You: ")
+#     if prompt.lower() == "exit":
+#         break
+
+#     response = discuss_with_gemini(prompt=prompt, prevChat=chat)
+#     chat = response["chat"]
+#     print(f"gemini: ", end="")
+#     print(response["text"])
+
+
+chat = None
+while True:
+    print("")
+    prompt = input("You: ")
+    if prompt.lower() == "exit":
+        break
+
+    response = discuss_with_gemini_stream(prompt=prompt, prevChat=chat)
+    print(f"gemini: ", end="")
+    for chunk in response:
+        print(chunk["text"], end="")
+        chat = chunk["chat"]
